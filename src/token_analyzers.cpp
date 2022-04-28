f9f769fccda7f6155d222dad0b72e98e0e4c51e9
Hoang Giang VO <hoang-giang.vo@epitech.eu>
2022-04-28 12:49:08 +0900

    open/read file - fix string analyzer - adding iostream import to hpp

--- src/token_analyzers.cpp
+++ src/token_analyzers.cpp
@@ -1,5 +1,4 @@
 #include "../headers/lexical_analyzer.hpp"
-#include <iostream>
 
 bool vtype_analyzer(const std::string &input, std::size_t &index, Scanner &scanner)
 {
@@ -16,26 +15,27 @@ bool string_analyzer(const std::string &input, std::size_t &index, Scanner &scan
     // set starting/ending idx of string's value between symbol '"'
     std::size_t idxStringBegin(0);
     std::size_t idxStringEnd(0);
-
-    if (input.at(std::size_t(index)) == '"') {
+    std::size_t tmpIndex = index;
+            
+            
+    if (input.at(std::size_t(tmpIndex)) == '"') {
         char c;
         // setting starting pos of string's value after symbol '"'
-        idxStringBegin = std::size_t(index + 1);
+        idxStringBegin = std::size_t(tmpIndex + 1);
         // skipping starting symbol of string in order
         // to parse the string value
-        index++;
+        tmpIndex++;
         // for each iteration we will :
         // - checking for the ending symbol, if there is one then we break
         // - checking for any combinations of digits, English letters, and blanks
-        while (index < input.length()) {
-            c = input[index];
+        while (tmpIndex < input.length()) {
+            c = input[tmpIndex];
             if (c == '"') {
-                idxStringEnd = std::size_t(index);
+                idxStringEnd = std::size_t(tmpIndex);
                 break;
             }
-            if (c == ' ' || (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') ||
-                (c >= 'A' || c <= 'Z'))
-                index++;
+            if (c == ' ' || isdigit(c) || isalpha(c))
+                    tmpIndex++;
             else
                 return false;
         }
@@ -44,11 +44,12 @@ bool string_analyzer(const std::string &input, std::size_t &index, Scanner &scan
 
     // Once the input is parsed, we will make sure that
     // we're ending with a symbol '"' at the end
-    if (input.at(std::size_t(index)) != '"' || idxStringEnd == static_cast<size_t>(0))
+    if (input.at(std::size_t(tmpIndex)) != '"' || idxStringEnd == static_cast<size_t>(0))
         return false;
     
     // Incrementing index in order to parse for the next symbol in the main loop
-    index++;
+    tmpIndex++;
+    index = tmpIndex;
 
     Token newToken = {"STRING", std::string(input.begin() + idxStringBegin, input.begin() + idxStringEnd)};
     scanner.tokens.push_back(newToken);
