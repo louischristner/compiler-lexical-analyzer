5c2f206cfcaa32deb4a694fde8ace942bdd6d393
Louis Christner <louis.christner@epitech.eu>
2022-04-28 06:20:26 +0200

    Update lexical_analyzer loop

--- src/lexical_analyzer.cpp
+++ src/lexical_analyzer.cpp
@@ -23,13 +23,15 @@ void lexical_analyzer(const std::string &input, Scanner &scanner)
         comma_analyzer
     };
 
+    std::size_t func_ptrs_size = sizeof(func_ptrs) / sizeof(func_ptrs[0]);
+
     for (std::size_t index = 0; index < input.size(); tokenFound = false) {
-        for (auto &func_ptr : func_ptrs) {
-            if ((tokenFound = func_ptr(input, index, scanner)))
+        while (input[index] == ' ' || input[index] == '\n' || input[index] == '\t')
+            index++;
+
+        for (std::size_t i_func_ptr = 0; i_func_ptr < func_ptrs_size && index < input.size(); i_func_ptr++)
+            if ((tokenFound = func_ptrs[i_func_ptr](input, index, scanner)))
                 break;
-            if (index >= input.size())
-                break;
-        }
 
         if (!tokenFound) {
             throw LexicalErrorException("Error");
